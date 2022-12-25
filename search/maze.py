93f4b96ddc79303a5be6e69295ddaa7a0032c8a4
Abuu254 <abubakarramadhan254@gmail.com>
2022-12-25 13:38:25 -0500

    Implimented file reading in maze

--- search/maze.py
+++ search/maze.py
@@ -42,7 +42,38 @@ class QueueFrontier(StackFrontier):
 class Maze():
     def __init__(self, filename):
         with open(filename) as f:
-            
+            # Validate start and goal
+        if contents.count("A") != 1:
+            raise Exception("maze must have exactly one start point")
+        if contents.count("B") != 1:
+            raise Exception("maze must have exactly one goal")
+
+        # Determine height and width of maze
+        contents = contents.splitlines()
+        self.height = len(contents)
+        self.width = max(len(line) for line in contents)
+
+        # Keep track of walls
+        self.walls = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                try:
+                    if contents[i][j] == "A":
+                        self.start = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == "B":
+                        self.goal = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == " ":
+                        row.append(False)
+                    else:
+                        row.append(True)
+                except IndexError:
+                    row.append(False)
+            self.walls.append(row)
+
+        self.solution = None
 
 
     def solve(self):
