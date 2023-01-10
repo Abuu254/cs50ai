1368c5e162879fcac036eac9f91e1dc1547514d1
Abuu254 <90156490+Abuu254@users.noreply.github.com>
2023-01-10 13:00:04 -0500

    Update emptyfolders.py

--- emptyfolders.py
+++ emptyfolders.py
@@ -1,22 +1,22 @@
 Import os
 
-// get root folder
+# get root folder
 
 root = input("Provide directory to search: ")
 save = input("Where to save the empty folder: ")
 
-// storing empty directories
+# storing empty directories
 
 empty = []
 
 for root, dirs, files in os.walk(root):
 
-// check size
+# check size
    if not len(dir) and not len(files):
       empty.append(root)
 
 
-// Writing to file
+# Writing to file
 file_name = 'empty.txt'
 with open(os.path.join(save, file_name), 'w') as fp:
    fp.write('\n'.join(empty))
