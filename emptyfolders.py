cd2e32d4fd1cb88692746b7d6f45fb406d41bd48
Abuu254 <90156490+Abuu254@users.noreply.github.com>
2023-01-10 13:07:26 -0500

    Update emptyfolders.py

--- emptyfolders.py
+++ emptyfolders.py
@@ -6,12 +6,10 @@ root = input("Provide directory to search: ")
 save = input("Where to save the empty folder: ")
 
 # storing empty directories
-
 empty = []
 
+# check size of folders
 for root, dirs, files in os.walk(root):
-
-# check size
    if not len(dir) and not len(files):
       empty.append(root)
 
